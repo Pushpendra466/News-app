7c26afd002a8743deec666815e615ccafdd87a24
Pushpendra Kumar <pushpendra466@gmail.com>
2020-08-30 00:43:49 +0530

    Updates

---
+++ precache-manifest.8c1f81182dc814479adc06ff62621baf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "465a67aba84e57c87bf47c5557b53950",
+    "url": "/News-app/index.html"
+  },
+  {
+    "revision": "91cee8d9cd81986131c0",
+    "url": "/News-app/static/css/2.cc2d33b2.chunk.css"
+  },
+  {
+    "revision": "1bde16dc99a0e2fa6829",
+    "url": "/News-app/static/css/main.cb082ad0.chunk.css"
+  },
+  {
+    "revision": "91cee8d9cd81986131c0",
+    "url": "/News-app/static/js/2.8c8e3c6b.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/News-app/static/js/2.8c8e3c6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bde16dc99a0e2fa6829",
+    "url": "/News-app/static/js/main.c200a26b.chunk.js"
+  },
+  {
+    "revision": "c3324ac2ea5d2ca62df3",
+    "url": "/News-app/static/js/runtime-main.54baa3c5.js"
+  }
+]);
